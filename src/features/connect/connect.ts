b7c8ab4fbdc6e2fe2aa5b9f1623da1469abec0ea
Albert Arakelyan <albertarakelyan1998@gmail.com>
2024-08-04 12:41:22 +0400

    code: added docstrings for connect.ts

--- src/features/connect/connect.ts
+++ src/features/connect/connect.ts
@@ -10,15 +10,57 @@ interface IFirebaseConfig {
   measurementId?: string;
 }
 
+/**
+ * Represents the configuration for Firebase.
+ * @param {string} apiKey - API key for Firebase.
+ * @param {string} authDomain - Domain for Firebase authentication.
+ * @param {string} projectId - Project ID for Firebase.
+ * @param {string} storageBucket - Storage bucket for Firebase.
+ * @param {string} messagingSenderId - Messaging Sender ID for Firebase.
+ * @param {string} appId - App ID for Firebase.
+ * @param {string} [measurementId] - Measurement ID for Firebase (optional).
+ */
 class FirebaseConfig {
+  /**
+   * API key for Firebase.
+   */
   apiKey: string;
+  /**
+   * Domain for Firebase authentication.
+   */
   authDomain: string;
+  /**
+   * Project ID for Firebase.
+   */
   projectId: string;
+  /**
+   * Storage bucket for Firebase.
+   */
   storageBucket: string;
+  /**
+   * Messaging Sender ID for Firebase.
+   */
   messagingSenderId: string;
+  /**
+   * App ID for Firebase.
+   */
   appId: string;
+  /**
+   * Measurement ID for Firebase (optional).
+   */
   measurementId?: string;
 
+  /**
+   * Constructs a new FirebaseConfig object.
+   * @param {Object} config - Configuration object.
+   * @param {string} config.apiKey - API key for Firebase.
+   * @param {string} config.authDomain - Domain for Firebase authentication.
+   * @param {string} config.projectId - Project ID for Firebase.
+   * @param {string} config.storageBucket - Storage bucket for Firebase.
+   * @param {string} config.messagingSenderId - Messaging Sender ID for Firebase.
+   * @param {string} config.appId - App ID for Firebase.
+   * @param {string} [config.measurementId] - Measurement ID for Firebase (optional).
+   */
   constructor({
     apiKey,
     authDomain,
@@ -38,6 +80,12 @@ class FirebaseConfig {
   }
 }
 
+/**
+ * Connects to Firebase using the provided configuration and returns a promise that resolves with the initialized app or rejects with an error message.
+ *
+ * @param {FirebaseConfig} firebaseConfig - The configuration object for initializing the Firebase app.
+ * @return {Promise<FirebaseApp>} A promise that resolves with the initialized Firebase app or rejects with an error message.
+ */
 const connect = (firebaseConfig: FirebaseConfig) => {
   return new Promise((resolve, reject) => {
     const app = initializeApp(firebaseConfig);
